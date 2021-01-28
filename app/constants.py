b41b5a1bf19d4c8b62582f3816ff18aba2b5f27a
Moldavskiy Denis <damoldavskiy@edu.hse.ru>
2021-01-28 10:04:44 +0300

    Fix

--- app/constants.py
+++ app/constants.py
@@ -4,3 +4,10 @@ ERROR_LOG_PATH = '/home/denis/logs/error.log'
 PIDFILE_PATH = '/home/denis/runtime.pid'
 
 TOKEN_SIZE = 64
+
+# Gunicorn
+
+daemon = True
+pidfile = PIDFILE_PATH
+errorlog = ERROR_LOG_PATH
+accesslog = ACCESS_LOG_PATH
